de7b365ded61b3d0ccbf2576831b0a5cdeefb3ec
Jorge <jorge@liferescuer.com>
2015-05-26 21:27:03 +0200

    Register and logout added

--- www/scripts/controllers/main.js
+++ www/scripts/controllers/main.js
@@ -2,5 +2,10 @@
 
 angular.module('sharoodApp')
   .controller('MainCtrl', function ($scope, sharoodDB) {
-  	sharoodDB.login();
+  	sharoodDB.login("jorgepruden@gmail.com", "test").then(function(user){
+  		console.info(user);
+	   	sharoodDB.logout().then(function(user){
+	  		console.info("Logout done!");
+	  	});
+  	});
   });
