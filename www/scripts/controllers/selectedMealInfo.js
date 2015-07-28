afac1cf62ae1e16287f4dc89f50ac23454c4cb71
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-07-28 11:58:36 +0200

    cancel meal

--- www/scripts/controllers/selectedMealInfo.js
+++ www/scripts/controllers/selectedMealInfo.js
@@ -11,13 +11,22 @@ define(['controllers/module'], function (controllers, AlertHelper) {
             return;
         }
 
-        var mealInfo = MealService.getCurrentMeal();
-
-        $scope.meal = mealInfo;
+        $scope.meal = MealService.getCurrentMeal();
 
         console.info($scope.meal);
 
         $scope.navigate = navigation.navigate;
+
+        $scope.cancel = function() {
+            console.info($scope.meal);
+            var cookies = $scope.meal.cookies_value;
+            var owner = $scope.meal.owner[0].uid;
+            var currentUser = $scope.currentUser.uid;
+
+            sharoodDB.transferCookies(owner, currentUser, cookies).then(function() {
+                // TODO
+            });
+        }
 
     });
 
